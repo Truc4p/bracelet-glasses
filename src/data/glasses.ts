ae937bdb7a2bebcc8e1f27f85cf0be29a729e92b
Truc1p <truc4pham@gmail.com>
2026-03-31 22:41:30 +0700

    commit

--- src/data/glasses.ts
+++ src/data/glasses.ts
@@ -7,11 +7,6 @@ export interface FrameEntry {
   images: string[];
 }
 
-const STATIC_FRAMES = [
-  { id: "black-out", name: "Black Out", dimensions: "52□23-149", description: "", images: ["/glasses/frames/BLACK_OUT.jpg"] },
-  { id: "blonde-clear", name: "Blonde Clear", dimensions: "53□23-150", description: "", images: ["/glasses/frames/Blonde_Clear.jpg"] },
-  { id: "light-grey", name: "Light Grey", dimensions: "51□24-149", description: "", images: ["/glasses/frames/Light_Grey.jpg"] },
-  { id: "tortoise-out", name: "Tortoise Out", dimensions: "53□24-149", description: "", images: ["/glasses/frames/TORTOISE_OUT.jpg"] },
-];
+const STATIC_FRAMES: FrameEntry[] = [];
 
 export const DEFAULT_FRAMES: FrameEntry[] = STATIC_FRAMES;
